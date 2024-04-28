b06c448de3259902823ed37e320c621cd1485370
Sertan <sertanozcan.tr@gmail.com>
2024-04-28 23:54:18 +0300

    Add useLocation

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,30 +1,29 @@
 import { Grid } from '@mui/material';
 import React from 'react';
 import Sidebar from '../../components/Sidebar';
-
+import { useLocation } from 'react-router-dom';
 
 const HomePage = () => {
+  const location = useLocation(); 
+
   return (
     <div>
-        <div className='px-20'>
-          <Grid container spacing={0}>
-            <Grid item xs={0} lg={3}> 
+      <div className='px-20'>
+        <Grid container spacing={0}>
+          <Grid item xs={0} lg={3}>
 
             <div className='stick top-0'>
-
-
               <Sidebar />
-
-          
-
-                
-
             </div>
 
-            </Grid>
-
           </Grid>
-        </div>
+
+          <Grid lg={location.pathname === "/" ? 6 : 9} item className='px-5 flex justify-center' xs={12}> 
+          
+          </Grid>
+
+        </Grid>
+      </div>
     </div>
   );
 };
