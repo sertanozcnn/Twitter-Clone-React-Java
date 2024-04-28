3431fab4bc26d4f86580a3219b53a8982ef67ec9
Sertan <sertanozcan.tr@gmail.com>
2024-04-28 23:42:17 +0300

    Home Page Create Update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,28 @@
+import { Grid } from '@mui/material';
 import React from 'react';
 
 
 const HomePage = () => {
   return (
     <div>
-        dfhfdgh
+        <div className='px-20'>
+          <Grid container spacing={0}>
+            <Grid item xs={0} lg={3}> 
+
+            <div className='stick top-0'>
+
+
+
+          
+
+                
+
+            </div>
+
+            </Grid>
+
+          </Grid>
+        </div>
     </div>
   );
 };
