81b9e9379b50ea63df7614ea052ccfee84d99776
Sertan <sertanozcan.tr@gmail.com>
2024-04-29 23:53:51 +0300

    Auth Test

--- src/Redux/Auth/auth.reducer.js
+++ src/Redux/Auth/auth.reducer.js
@@ -12,19 +12,19 @@ export const authReducer = (state = initalState, action) => {
     switch (action.type) {
         case LOGIN_REQUEST:
         case REGISTER_SUCCESS:
-            return { ...state, loading: true, error: null }
+            return { ...state, loading: true, error: null };
 
         case LOGIN_SUCCESS:
         case REGISTER_SUCCESS:
-            return { ...state, jwt: action.payload, loading: false, error: null }
+            return { ...state, jwt: action.payload, loading: false, error: null };
 
         case LOGIN_FAILURE:
         case REGISTER_FAILURE:
-            return { ...state, loading: false, error: action.payload }
+            return { ...state, loading: false, error: action.payload };
 
         default:
             return state;
 
     }
 
-}
+};
--- src/components/Sidebar/SidebarNavigation.js
+++ src/components/Sidebar/SidebarNavigation.js
@@ -14,42 +14,42 @@ export const navigationMenu = [
 
     {
         title: "Home",
-        icon: <BiHomeCircle  size={32} />,
+        icon: <BiHomeCircle  size={28} />,
         path: "/",
     },
     {
         title: "Reels",
-        icon: <MdOutlineExplore   size={30} />,
+        icon: <MdOutlineExplore   size={28} />,
         path: "/",
     },
     {
         title: "Create Reels",
-        icon: <MdAddCircleOutline   size={28}/>,
+        icon: <MdAddCircleOutline   size={26}/>,
         path: "/",
     },
     {
         title: "Notifications",
-        icon: <MdOutlineNotificationsNone  size={30} />,
+        icon: <MdOutlineNotificationsNone  size={28} />,
         path: "/",
     },
     {
         title: "Message",
-        icon: <BiMessageDetail  size={28} />,
+        icon: <BiMessageDetail  size={26} />,
         path: "/",
     },
     {
         title: "Lists",
-        icon: <RiFileListLine  size={28} />,
+        icon: <RiFileListLine  size={26} />,
         path: "/",
     },
     {
         title: "Communities",
-        icon: <RiGroupLine  size={28} />,
+        icon: <RiGroupLine  size={26} />,
         path: "/",
     },
     {
         title: "Profile",
-        icon: <MdOutlineAccountCircle  size={30} />,
+        icon: <MdOutlineAccountCircle  size={28} />,
         path: "/",
     },
 
